5ac0dfd8bfd0b72f1488959c87d860aff456e88e
abdelhaleemosama <117584998+abdelhaleemosama@users.noreply.github.com>
2026-09-19 20:59:36 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int p1=2,p2=1;
+        for(int i=2;i<n;i++){
+            int curr = p1+p2;
+            p2=p1;
+            p1=curr;
+        }
+        return p1;
+    }
+}
